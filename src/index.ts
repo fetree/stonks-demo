f0cd3bdafccf3189f9d549177ade4542b14bba0e
David Eisenbaum <davidjeisenbaum@gmail.com>
2024-06-02 23:46:00 -0700

    basic stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+require('dotenv').config();
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { OAuth2Client } from 'google-auth-library';
 
 const prisma = new PrismaClient();
 
@@ -10,67 +14,101 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
-  res.json(todos);
+// Auth
+app.post("/signUpEmail", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: { email, password: passwordHash },
+    });
+
+    const token = jwt.sign({ email: user.email, id: user.id }, process.env.SECRET_KEY!, { expiresIn: '1h' });
+
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
-    data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
-    },
-  });
+app.post("/signInEmail", async (req, res) => {
+  const { email, password } = req.body;
 
-  return res.json(todo);
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+    if (!user || !user.password) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ email: user.email, id: user.id }, process.env.SECRET_KEY!, { expiresIn: '1h' });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
+app.post('/googleSignup', async (req, res) => {
+  const { token } = req.body;
 
-  return res.json(todo);
-});
+  if (!token) {
+    return res.status(400).json({ message: 'Token is required' });
+  }
 
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
+  try {
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
 
-  return res.json(todo);
-});
+    if (!payload || !payload.email) {
+      return res.status(400).json({ message: 'Invalid token' });
+    }
 
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
+    const { email } = payload;
 
-  return res.send({ status: "ok" });
-});
+    let user = await prisma.user.findUnique({ where: { email } });
 
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Todo REST API</h1>
-  <h2>Available Routes</h2>
-  <pre>
-    GET, POST /todos
-    GET, PUT, DELETE /todos/:id
-  </pre>
-  `.trim(),
-  );
+    if (!user) {
+      user = await prisma.user.create({
+        data: { email, password: '' },
+      });
+    }
+
+    const jwtToken = jwt.sign({ email: user.email, id: user.id }, process.env.SECRET_KEY!, { expiresIn: '1h' });
+
+    res.status(200).json({ token: jwtToken });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 app.listen(Number(port), "0.0.0.0", () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
