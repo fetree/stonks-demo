b3ee764e4639f18250da7d4bb0ca0ba1b600bdfe
David Eisenbaum <davidjeisenbaum@gmail.com>
2024-06-03 16:48:18 -0700

    emails

--- src/channel.ts
+++ src/channel.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { Router } from "express";
 import { authenticateToken } from "./middleware";
+import { sendEmail } from "./email";
 
 const prisma = new PrismaClient();
 
@@ -30,6 +31,45 @@ router.post('/createChannel', authenticateToken, async (req, res) => {
                 creatorId: user.id,
             },
         });
+
+        // Send notification to followers, will just do email right now
+        const [activeFollowers, inactiveFollowers] = await Promise.all([prisma.follow.findMany({
+            where: {
+                followingId: user.id,
+                follower: {
+                    active: true,
+                },
+            },
+            include: {
+                follower: true,
+            },
+        }), prisma.follow.findMany({
+            where: {
+                followingId: user.id,
+                follower: {
+                    active: false,
+                },
+            },
+            include: {
+                follower: true,
+            },
+        })]);
+
+        activeFollowers.forEach(f => {
+            const email = f.follower.email;
+            const subject = `${user.username} just went live!`;
+            const text = `Hey! ${f.follower.username},\n\nA new channel "${title}" has been started`;
+            sendEmail(email, subject, text);
+        });
+
+        // Send emails to inactive followers
+        inactiveFollowers.forEach(f => {
+            const email = f.follower.email;
+            const subject = 'Offline user get back on!';
+            const text = `Hey! ${f.follower.username},\n\nWe've created a new channel titled "${title}" hop on stonks to watch!`;
+            sendEmail(email, subject, text);
+        });
+
 
         res.status(201).json(channel);
     } catch (error) {
